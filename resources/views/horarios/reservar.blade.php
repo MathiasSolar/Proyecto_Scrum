f39218a04cbb380f85ba78a77d39c264edd299c7
Mathias <mathiassolar24@gmail.com>
2023-05-22 15:33:51 -0400

    avance de creacion datos de horario

--- resources/views/horarios/reservar.blade.php
+++ resources/views/horarios/reservar.blade.php
@@ -5,6 +5,10 @@
 
     <form action="{{ route('horarios.guardarReserva', $horario->id) }}" method="POST">
         @csrf
+        <div class="form-group">
+            <label for="rut">Rut:</label>
+            <input type="text" name="rut" class="form-control" required>
+        </div>
 
         <div class="form-group">
             <label for="nombre">Nombre:</label>
@@ -19,6 +23,11 @@
         <div class="form-group">
             <label for="email">Correo Electrónico:</label>
             <input type="email" name="email" class="form-control" required>
+        </div>
+
+        <div class="form-group">
+            <label for="carrera">Carerra:</label>
+            <input type="text" name="carrera" class="form-control" required>
         </div>
 
         <button type="submit" class="btn btn-primary">Reservar</button>
